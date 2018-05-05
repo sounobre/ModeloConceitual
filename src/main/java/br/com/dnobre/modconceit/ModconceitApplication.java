d1f99379e51ae34759c9e8c12f849fce0e2594ef
Diego Nobre <sounobre@hotmail.com>
2018-05-05 18:34:24 -0300

    Projeto criado

---
+++ src/main/java/br/com/dnobre/modconceit/ModconceitApplication.java
@@ -0,0 +1,12 @@
+package br.com.dnobre.modconceit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModconceitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModconceitApplication.class, args);
+	}
+}
