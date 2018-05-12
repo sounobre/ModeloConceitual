c50b2e51dbf0bcf42ac12ce743f5c72854d618c7
Diego Nobre <sounobre@hotmail.com>
2018-05-12 20:03:41 -0300

    Estado e Cidade

--- src/main/java/br/com/dnobre/modconceit/ModconceitApplication.java
+++ src/main/java/br/com/dnobre/modconceit/ModconceitApplication.java
@@ -1,14 +1,18 @@
 package br.com.dnobre.modconceit;
 
 import br.com.dnobre.modconceit.domain.Categoria;
+import br.com.dnobre.modconceit.domain.Cidade;
+import br.com.dnobre.modconceit.domain.Estado;
 import br.com.dnobre.modconceit.domain.Produto;
 import br.com.dnobre.modconceit.repositories.CategoriaRepository;
-import br.com.dnobre.modconceit.repositories.ProdutoRepository;
+import br.com.dnobre.modconceit.repositories.CidadeRepository;
+import br.com.dnobre.modconceit.repositories.EstadoRepository;
 import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import br.com.dnobre.modconceit.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ModconceitApplication implements CommandLineRunner{
@@ -18,6 +22,12 @@ public class ModconceitApplication implements CommandLineRunner{
         
         @Autowired
         ProdutoRepository produtoRepository;
+        
+        @Autowired
+        EstadoRepository estadoRepository;
+        
+        @Autowired
+        CidadeRepository cidadeRepository;
     
 	public static void main(String[] args) {
 		SpringApplication.run(ModconceitApplication.class, args);
@@ -39,8 +49,24 @@ public class ModconceitApplication implements CommandLineRunner{
         p2.getCategoria().addAll(Arrays.asList(cat1,cat2));
         p3.getCategoria().addAll(Arrays.asList(cat1));
         
+       
         categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
         
         produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+        
+        
+         Estado est1 = new Estado(null, "Minas Gerais");
+        Estado est2 = new Estado(null, "São Paulo");
+        
+        Cidade c1 = new Cidade(null, "Uberlândia", est1);
+        Cidade c2 = new Cidade(null, "São Paulo", est2);
+        Cidade c3 = new Cidade(null, "Campinas", est2);
+        
+        est1.getCidades().addAll(Arrays.asList(c1));
+        est2.getCidades().addAll(Arrays.asList(c2, c3));
+        
+        estadoRepository.saveAll(Arrays.asList(est1, est2));
+        cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+        
     }
 }
