d591749c787452321cb7638f9cf0c5a88367efae
Diego Nobre <sounobre@hotmail.com>
2018-05-12 15:27:13 -0300

    Criando repositorio e service para categoria

---
+++ src/main/java/br/com/dnobre/modconceit/services/CategoriaService.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.dnobre.modconceit.services;
+
+import br.com.dnobre.modconceit.domain.Categoria;
+import br.com.dnobre.modconceit.repositories.CategoriaRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author souno
+ */
+@Service
+public class CategoriaService {
+    
+    @Autowired
+    private CategoriaRepository repo;
+    
+    public Categoria buscar(Integer id){
+        
+        Optional<Categoria> obj = repo.findById(id);
+        
+        return obj.orElse(null);
+    }
+}
