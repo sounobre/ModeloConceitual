990f4fd260fad192196cc1f3b2fa897ea0b9b485
Diego Nobre <sounobre@hotmail.com>
2018-07-22 20:34:44 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/br/com/dnobre/modconceit/services/CategoriaService.java
+++ src/main/java/br/com/dnobre/modconceit/services/CategoriaService.java
@@ -13,6 +13,9 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 /**
@@ -55,5 +58,10 @@ public class CategoriaService {
         
         public List<Categoria> findAll(){
             return repo.findAll();
+        }
+        
+        public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+            PageRequest pageRequest = new PageRequest(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+            return repo.findAll(pageRequest);
         }
 }
